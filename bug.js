8a5d5731d80a7b32ff1adca7ec5da3a1d2db6bc3
BugHunter-X <askzenai@gmail.com>
2025-01-09 21:50:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+function foo(a,b){    if(a === 0 || b === 0) {        return 0;    }    return a/b;}
+
+console.log(foo(1,0)); // Throws an error
+console.log(foo(0,1)); // Throws an error
+console.log(foo(1,1)); // Returns 1
+console.log(foo(5,2)); // Returns 2.5
